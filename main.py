a00c153cddd6477039522216fe776773b4d5e98b
NinjaRla <apples4421@gmail.com>
2026-05-13 09:38:34 -0700

    Added error handling for both .get and .patch, and updated the .patch so it utilizes the phrase_id for a cleaner lookup/implementation.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from db import connect_to_db
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException
 from models import PhraseRequest, PhraseResponse
 from typing import Annotated, Literal
+from bson.objectid import ObjectId
+import bson.errors
 
 collection = None
 
@@ -25,6 +27,7 @@ Notes on how to implement it:
 - Motor's find_one takes a filter dict as the first argument?
 - Sorting by num_uses ascending before grabbing one result. Motor has a sort method. """
 
+
 @app.get("/phrase/")
 async def get_phrase(filter_query: Annotated[PhraseRequest, Query()]):
     '''
@@ -39,31 +42,48 @@ async def get_phrase(filter_query: Annotated[PhraseRequest, Query()]):
     else:
         phrase = await collection.find_one({}, sort=[("num_uses", 1)])
 
+    # error handling for when there is no phrase found in the query category 
+    if phrase is None:
+        if filter_query.category:
+            raise HTTPException(status_code=404, detail=f"There are no phrase found for the category: {filter_query.category}.")
+
+        # when there is no phrase within the database (for user story 1 where the microservice selects the fact with the lowest num_uses value.)
+        raise HTTPException(status_code=404, detail="There are no phrase found in the database.")
+
     # if ObjectId is not automatically jsonifiable
     phrase["_id"] = str(phrase["_id"])
 
     return phrase
 
 
-
-
-
 @app.patch("/phrase/")
-async def update_num_uses(filter_query: Annotated[PhraseRequest, Query()]):
+async def update_num_uses(phrase_id: str):
     '''
     Update num_uses by 1 after getting a phrase.
     '''
-    # will be using the same logic to find the correct id for the fact
 
-    if filter_query.category:
-        phrase = await collection.find_one({"category": filter_query.category}, sort=[("num_uses", 1)])
-    else:
-        phrase = await collection.find_one({}, sort=[("num_uses", 1)])
+    # error handling for incorrect phrase_ids 
+    try:
+        object_id = ObjectId(phrase_id)
+    except bson.errors.InvalidID:
+        raise HTTPException(status_code=400, detail=f"Conversion failed. Invalid ID format: {phrase_id}")
 
-    # update the num_uses for the selected fact
-    updated = await collection.update_one({"_id": phrase["_id"]}, {"inc": {"num_uses", 1}})
+    updated = await collection.update_one({"_id" : object_id}, {"$inc": {"num_uses", 1}})
 
+    # error for when the input phrase_id is valid but not found within the database.
+
+    if updated.matched_count == 0:
+        raise HTTPException(status_code=404, detail=f"There is no phrase found in the database with the phrase ID: {phrase_id}.")
+    
+    # if the phrase is found but no changes are made
+    if updated.modified_count != 1:
+        raise HTTPException(status_code=500, detail=f"The phrase with the phrase ID: {phrase_id} has been found but its num_uses has not been updated.")
 
     # return the matched and modified count number. 
-    return {"updated.matched_count: " : updated.matched_count, "updated.modified_count: " : updated.modified_count}
+    return { 
+        "phrase_id" : phrase_id,
+        "updated.matched_count: " : updated.matched_count, 
+        "updated.modified_count: " : updated.modified_count
+        }
+
 
